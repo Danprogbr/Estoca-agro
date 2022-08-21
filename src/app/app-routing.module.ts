b156cdd269f2648f8f21102e5dae864c7971721a
daniellykammers <dany.kammers@hotmail.com>
2022-08-21 11:01:57 -0300

    feat(home): style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { Tela2Component } from './componentes/tela2/tela2.component';
+import { ResultComponent } from './containers/result/result.component';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./componentes/tela1/tab1.module').then((m) => m.Tab1PageModule),
-  },
-  {
-    path: 'tela2',
-    component: Tela2Component,
+      import('./componentes/home/tab1.module').then((m) => m.Tab1PageModule),
   },
   {
     path: 'result/:type',
