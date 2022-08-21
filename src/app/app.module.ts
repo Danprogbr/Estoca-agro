0a5df140f4b055c0a2b2fd5fa02cd07aa0ecba76
danprogbr <programador.vff@gmail.com>
2022-08-21 11:33:58 -0300

    ft(step) - create a new step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Form1Component} from "./componentes/forms/form1/form1.component";
+import {Form2Component} from "./componentes/forms/form2/form2.component";
+import {Form3Component} from "./componentes/forms/form3/form3.component";
+import {Form4Component} from "./componentes/forms/form4/form4.component";
+import {StepComponent} from "./componentes/step/step.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [
+    AppComponent,
+    Form1Component,
+    Form2Component,
+    Form3Component,
+    Form4Component,
+    StepComponent,
+  ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/componentes/home/tab1.module.ts
+++ src/app/componentes/home/tab1.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
   ],
-  declarations: [Tab1Page]
+  declarations: [
+    Tab1Page,
+  ]
 })
 export class Tab1PageModule {}
